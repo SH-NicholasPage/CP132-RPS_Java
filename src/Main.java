aed6baa6f042dad53cef48a9dadf86fa0cb1a051
Nicholas Page <npage@southhills.edu>
2024-10-09 16:25:17 -0400

    Added a Random instance

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@
  * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
  */
 
+import java.util.Random;
+
 public class Main
 {
+    public static Random random = new Random();
+
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
